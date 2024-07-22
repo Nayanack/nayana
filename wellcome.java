edb3c54e263531a242b3d4fcfc686ef000eef268
Nayanack <159225065+Nayanack@users.noreply.github.com>
2024-07-22 10:22:04 +0530

    Update wellcome.java

--- wellcome.java
+++ wellcome.java
@@ -3,5 +3,8 @@ public class wellcom
 	public static void main(String[]args)
 	{
 		System.out.println("wellcome to spt  fgadfasgydjkghasdha");
+		System.out.println("wellcome to spt  fgadfasgydjkghasdha");
+		System.out.println("wellcome to spt  fgadfasgydjkghasdha");
+		
 	}
 }
